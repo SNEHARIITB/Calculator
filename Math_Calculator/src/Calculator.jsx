b8b9cc9d7174ce442b5de20e853cc4abf9ca32c5
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-15 15:50:41 +0530

    initial commit

---
+++ Math_Calculator/src/Calculator.jsx
@@ -0,0 +1,10 @@
+
+import styles from "./Calculator.module.css";
+const Calculator = () => {
+    return(
+        <div className={styles.countainer}>
+
+        </div>
+    );
+}
+export default Calculator;
